dbdd9bb141c2975cf9a853245b6e347581e8e749
shashank <shashankhebbarm@gmail.com>
2023-12-05 11:30:46 +0530

    project restructure

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
