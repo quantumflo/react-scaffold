271412616d6a8ee7b79313eb33ef3deaef6254a7
shashank <shashankhebbarm@gmail.com>
2023-12-11 14:37:07 +0530

    Add sublocation

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is a food delivery app.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+    return (
+        <div className="contact">
+        <h1>Contact us</h1>
+        <p>Phone: +91 9#########</p>
+        <p>Email: reactscaffold@gmail.com</p>
+        </div>
+    );
+};
+
+export default Contact;
