337cd91e793dfa7401d3a053da17d1c602de4e11
shashank <shashankhebbarm@gmail.com>
2023-12-11 21:35:58 +0530

    lazy load

--- Index.js
+++ Index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './src/components/App';
-import About from './src/components/About';
-import Contact from './src/components/Contact';
 import Error from './src/components/Error';
 import Body from './src/components/Body';
 import RestoMenu from './src/components/RestoMenu';
+import Shimmer from './src/components/Shimmer';
+
+// import About from './src/components/About';
+// import Contact from './src/components/Contact';
+const About = lazy(() => import('./src/components/About'));
+const Contact = lazy(() => import('./src/components/Contact'));
 
 const router = createBrowserRouter([
     {
@@ -21,11 +25,11 @@ const router = createBrowserRouter([
         },
         {
             path: "/about",
-            element: <About />,
+            element: <Suspense fallback={<Shimmer />}><About /></Suspense>,
         },
         {
             path: "/contact",
-            element: <Contact />,
+            element: <Suspense fallback={<Shimmer />}><Contact /></Suspense>,
         },
         {
             path: "/restaurant/:resId",
