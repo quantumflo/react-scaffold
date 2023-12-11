8a6c0412e55de0cdc1cb4497e40ace0ddae617dc
shashank <shashankhebbarm@gmail.com>
2023-12-11 16:29:44 +0530

    fix footer at the bottom

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,10 @@
-
 const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>This is a food delivery app.</p>
-        </div>
-    );
-}
+  return (
+    <div style={{ minHeight: "65vh", padding: "5rem 0 0 5rem" }}>
+      <h1>About</h1>
+      <h2>This is a food delivery app.</h2>
+    </div>
+  );
+};
 
 export default About;
