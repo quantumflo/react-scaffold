a6622605b680a2bb971b94feaf1d980efdbeb08d
shashank <shashankhebbarm@gmail.com>
2023-12-11 15:58:33 +0530

    improve footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 
+
 const Footer = () => {
-    return (
-      <div className='footer'>
-        <div className='footer-container'>Footer</div>
+  return (
+    <footer className="footer-container">
+      <div className="footer-bottom">
+        <p>&copy; 2023 Food Delivery App. All rights reserved.</p>
       </div>
-    );
-}
+    </footer>
+  );
+};
 
 export default Footer;
