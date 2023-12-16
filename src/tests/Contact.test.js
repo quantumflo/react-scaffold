ee8eecd068fd19caa4832f91940eb608dc55488c
shashank <shashankhebbarm@gmail.com>
2023-12-16 22:12:32 +0530

    add test suite

--- src/tests/Contact.test.js
+++ src/tests/Contact.test.js
@@ -1,9 +1,11 @@
-import {render, screen } from '@testing-library/react';
-import Contact from '../components/Contact';
+import { render, screen } from "@testing-library/react";
+import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test('Contact component', () => {
+describe(" testing contact component ", () => {
+  test("Contact component", () => {
     render(<Contact />);
-    const contactElement = screen.getByRole('heading');
+    const contactElement = screen.getByRole("heading");
     expect(contactElement).toBeInTheDocument();
-})
+  });
+});
